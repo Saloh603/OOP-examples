b9622af5944a6b3f9c375bac11eecfc8ed6ad745
Salohiddin <woxqay777@gmail.com>
2024-01-30 17:23:26 +0500

    Person

--- examples.py
+++ examples.py
@@ -1,5 +1,5 @@
 import math
-
+from datetime import date
 
 class Circle:
     def __init__(self, radius):
@@ -12,7 +12,43 @@ class Circle:
         return 2 * math.pi * self.radius
 
 
-radius = float(input("Enter the radius of the circle: "))
-circle = Circle(radius)
-print("Area of the circle:", circle.area())
-print("Perimeter of the circle:", circle.perimeter())
+# radius = float(input("Enter the radius of the circle: "))
+# circle = Circle(radius)
+# print("Area of the circle:", circle.area())
+# print("Perimeter of the circle:", circle.perimeter())
+
+
+class Person:
+    def __init__(self, name, country, date_of_birth):
+        self.name = name
+        self.country = country
+        self.date_of_birth = date_of_birth
+
+    def calculate_age(self,):
+        today = date.today()
+        age = today.year - self.date_of_birth.year
+        if today < date(today.year, self.date_of_birth.month, self.date_of_birth.day):
+            age -= 1
+        return age
+
+person1 = Person("Ferdi Odilia", "France", date(1962, 7, 12))
+person2 = Person("Shweta Maddox", "Canada", date(1982, 10, 20))
+person3 = Person("Elizaveta Tilman", "USA", date(2000, 1, 1))
+
+print("Person 1:")
+print("Name:", person1.name)
+print("Country:", person1.country)
+print("Date of Birth:", person1.date_of_birth)
+print("Age:", person1.calculate_age())
+
+print("\nPerson 2:")
+print("Name:", person2.name)
+print("Country:", person2.country)
+print("Date of Birth:", person2.date_of_birth)
+print("Age:", person2.calculate_age())
+
+print("\nPerson 3:")
+print("Name:", person3.name)
+print("Country:", person3.country)
+print("Date of Birth:", person3.date_of_birth)
+print("Age:", person3.calculate_age())
