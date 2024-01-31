995fdfc4bca9f9a3066215b5c5fdbccd15a9a362
Salohiddin <woxqay777@gmail.com>
2024-01-31 11:17:02 +0500

    Person

--- examples.py
+++ examples.py
@@ -31,24 +31,49 @@ class Person:
             age -= 1
         return age
 
-person1 = Person("Ferdi Odilia", "France", date(1962, 7, 12))
-person2 = Person("Shweta Maddox", "Canada", date(1982, 10, 20))
-person3 = Person("Elizaveta Tilman", "USA", date(2000, 1, 1))
+# person1 = Person("Ferdi Odilia", "France", date(1962, 7, 12))
+# person2 = Person("Shweta Maddox", "Canada", date(1982, 10, 20))
+# person3 = Person("Elizaveta Tilman", "USA", date(2000, 1, 1))
+#
+# print("Person 1:")
+# print("Name:", person1.name)
+# print("Country:", person1.country)
+# print("Date of Birth:", person1.date_of_birth)
+# print("Age:", person1.calculate_age())
+#
+# print("\nPerson 2:")
+# print("Name:", person2.name)
+# print("Country:", person2.country)
+# print("Date of Birth:", person2.date_of_birth)
+# print("Age:", person2.calculate_age())
+#
+# print("\nPerson 3:")
+# print("Name:", person3.name)
+# print("Country:", person3.country)
+# print("Date of Birth:", person3.date_of_birth)
+# print("Age:", person3.calculate_age())
 
-print("Person 1:")
-print("Name:", person1.name)
-print("Country:", person1.country)
-print("Date of Birth:", person1.date_of_birth)
-print("Age:", person1.calculate_age())
 
-print("\nPerson 2:")
-print("Name:", person2.name)
-print("Country:", person2.country)
-print("Date of Birth:", person2.date_of_birth)
-print("Age:", person2.calculate_age())
+class Calculator:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-print("\nPerson 3:")
-print("Name:", person3.name)
-print("Country:", person3.country)
-print("Date of Birth:", person3.date_of_birth)
-print("Age:", person3.calculate_age())
+    def add(self):
+        return self.x + self.y
+
+    def subtract(self):
+        return self.x - self.y
+
+    def multiply(self):
+        return self.x * self.y
+
+    def divide(self):
+        return self.x / self.y
+
+# calculator = Calculator(2, 3)
+print("Sum:", Calculator(8, 2).divide())
+# print("Difference:", calculator.subtract(2, 3))
+# print("Product:", calculator.multiply(2, 3))
+# print("Quotient:", calculator.divide(2, 3))
+
